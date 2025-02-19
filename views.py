36edf6bc446030b1fa91a84b95d78e3e967b3da9
Ohlweiler47 <danielcezar@live.com>
2025-02-18 23:34:29 -0300

    Initial commit

---
+++ views.py
@@ -0,0 +1,17 @@
+from flask import render_template
+
+from main import app
+
+#rotas
+
+@app.route("/" )
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/verdade")
+def verdade():
+    return render_template("verdade.html")
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return render_template("usuarios.html", nome_usuario=nome_usuario)
